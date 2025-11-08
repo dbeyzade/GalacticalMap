61d714c3b1becdb8727ef261aab4eb509f9ecff1
Dogukanbeyzade <dbeyzade@hotmail.com>
2025-11-08 05:33:23 +0300

    Initial Commit

---
+++ GalacticalMap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GalacticalMap
+//
+//  Created by dogukan beyzade on 8.11.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GalacticalMap/GalacticalMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalacticalMapApp.swift
+//  GalacticalMap
+//
+//  Created by dogukan beyzade on 8.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct GalacticalMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
